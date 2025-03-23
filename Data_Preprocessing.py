eb4bf1ad042fe40ba6a36675b14b76238c400a63
Alex <alexgrendell@hotmail.com>
2025-03-23 22:47:43 +0000

    23/03/2025 - Fixed problem of getting lenght in BreastCancerDataset - Implemented the Res Net CNN with my own custom parameter design - Implemented model training, and accuracy / loss report (getting good results, although processing speed is a problem) - Trained the model on out dataset (it worked)

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import os
-
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 
-# Define transformations (resize, convert to tensor, normalize)
+from PIL import Image
+
+# Define transformations:
+# Resixe images to 224 x 224 (A common size for CNN's)
+# Convert images to PyTorch tensors
+# Normalise pixel values to [-1,1]
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize to 224x224
     transforms.ToTensor(),          # Convert to PyTorch tensor
@@ -15,15 +19,16 @@ transform = transforms.Compose([
 ])
 
 class BreastCancerDataset(Dataset):
-    
+    """A custom dataset for loading and labeling breast cancer images."""
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
-        self.image_paths = []
-        self.labels = []
+        self.image_paths = [] # Sotres image file paths
+        self.labels = [] # Store labels (0 = Benign, 1 = Malignant)
         
+        # Loop through all files in the dataset directory
         for img_name in os.listdir(self.root_dir):
-            if img_name.endswith('png'):
+            if img_name.endswith('png'): # Only want to process .png files (images)
                 img_path = os.path.join(root_dir, img_name)
                 self.image_paths.append(img_path)
                 
@@ -32,11 +37,30 @@ class BreastCancerDataset(Dataset):
                 else:
                     self.labels.append(0)  # Benign
                     
-    print("Class BreastCancerDataset is defined correctly")
+        print(f"Image Paths: {self.image_paths}")
+    def __len__(self):
+        """Return the total number of samples"""
+        
+        return len(self.image_paths)
+    
+    def __getitem__(self, index):
+        """Return a sample from the dataset"""
+        
+        img_path = self.image_paths[index]
+        image = Image.open(img_path).convert('RGB')
+        label = self.labels[index]
+        
+        # If the image hasn't been transformed, then transform it
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, label
+                    
+    #print("Class BreastCancerDataset is defined correctly")
     
 
-database_dir = "/Users/alexrendell/Documents/MSc - Advanced Computer Science/CSCM10-Computer_Science_Project_Research_Methods/Databases/BreakHis_BreastCancer"
+#database_dir = "/Users/alexrendell/Documents/MSc - Advanced Computer Science/CSCM10-Computer_Science_Project_Research_Methods/Databases/BreakHis_BreastCancer"
 
-dataset = BreastCancerDataset(database_dir, transform=transform)
+#dataset = BreastCancerDataset(database_dir, transform=transform)
 
-print(len(dataset.labels))
+#print(len(dataset.labels))
