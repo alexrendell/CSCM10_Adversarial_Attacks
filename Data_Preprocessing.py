466297be35955723ac8d5b31662427d2048d9bb9
Alex <alexgrendell@hotmail.com>
2025-07-24 19:56:00 +0100

    Uses Three RGB Channels
    Updates the preprocessing, ready for re-creating Madry 1

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -15,7 +15,10 @@ from PIL import Image
 transform = transforms.Compose([
     transforms.Resize((224, 224)),  # Resize to 224x224
     transforms.ToTensor(),          # Convert to PyTorch tensor
-    transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize to [-1,1]
+    # For 3 RGB channels
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+    #transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize to [-1,1]
 ])
 
 class BreastCancerDataset(Dataset):
